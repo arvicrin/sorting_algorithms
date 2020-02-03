bf865c95129fa14abc977be28e6759397e2e5b8c
danrocus1994 <danrodcastillo1994@gmail.com>
2020-02-03 14:26:01 -0500

    Ddsa

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,43 +1,32 @@
 #include "sort.h"
 /**
- *swap - sorting by selection algorithm
- *@array:the array
- *@pos: the pos to replace
- *@x: the minur pos in array
- */
-void swap(int *array, size_t pos, size_t x)
-{
-	int tmp = array[pos];
-
-	array[pos] = array[x];
-	array[x] = tmp;
-}
-/**
  *selection_sort - sorting by selection algorithm
  *@array:the array
  *@size: th array of the size
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t pos = 0, j = 0, minur = 0, x;
+	size_t pos = 0, j = 0, x;
+	int minur;
 
 	if (size < 2)
 		return;
-	for (; pos < size; pos++)
+	for (pos = 0; pos < size - 1; pos++)
 	{
 		minur = array[pos];
 		x = pos;
-		for (j = pos; j < size; j++)
+		for (j = pos + 1; j < size; j++)
 		{
-			if (array[j] < minur)
+			if (minur > array[j])
 			{
 				minur = array[j];
 				x = j;
 			}
 		}
-		if (x != pos)
+		array[x] = array[pos];
+		array[pos] = minur;
+		if (pos != x)
 		{
-			swap(array, pos, x);
 			print_array(array, size);
 		}
 	}
