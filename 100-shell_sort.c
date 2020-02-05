33afc45f8399244974b7712a2dcd94c085a51223
arvicrin <1231@holbertonschool.com>
2020-02-05 20:43:31 +0000

    add condition to array null or 1

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,6 +1,22 @@
 #include "sort.h"
 
 /**
+  * swap_s - swap two element of a list
+  * @array: array
+  * @i: index
+  * @j: j
+  * Return: nothing
+  */
+void swap_s(int *array, int i, int j)
+{
+	int copy_num = 0;
+
+	copy_num = array[i];
+	array[i] = array[j];
+	array[j] = copy_num;
+}
+
+/**
   * shell_sort - Selection sort
   * @array: array
   * @size: size
@@ -8,7 +24,10 @@
   */
 void shell_sort(int *array, size_t size)
 {
-	int gap = 1, i = 0, j = 0, si = size, save_num = 0;
+	int gap = 1, i = 0, j = 0, si = size;
+
+	if (array == NULL || size < 2)
+		return;
 
 	while (gap < si / 3)
 		gap = (gap * 3) + 1;
@@ -16,13 +35,12 @@ void shell_sort(int *array, size_t size)
 	while (gap > 0)
 	{
 		for (i = gap; i < si; i++)
-		{
-			save_num = array[i];
-			for (j = i; j >= gap && save_num < array[j - gap]; j -= gap)
-				array[j] = array[j - gap];
-			array[j] = save_num;
-		}
-
+			for (j = i; j > 0 && array[j] < array[j - gap]; j -= gap)
+			{
+				swap_s(array, j, j - gap);
+				if (j - gap - gap < 0)
+					break;
+			}
 		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
