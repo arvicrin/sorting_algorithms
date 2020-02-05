f604400719191fe2a9264dc579cdae8ed6b3e717
danrocus1994 <danrodcastillo1994@gmail.com>
2020-02-05 13:57:37 -0500

    dsa

---
+++ 102-counting_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+  * counting_sort - Selection sort
+  * @array: array
+  * @size: size
+  * Return: nothing
+  */
+void counting_sort(int *array, size_t size)
+{
+	int *counter = NULL, max_num = array[0], i = 0, si = size;
+
+	for (i = 0; i < si; i++)
+		if (array[i] > max_num)
+			max_num = array[i];
+
+	counter = malloc(sizeof(int) * (max_num + 1));
+	if (!counter)
+		return;
+
+	for (i = 0; i < max_num + 1; i++)
+		counter[i] = 0;
+
+	for (i = 0; i < si; i++)
+		counter[array[i]] += 1;
+
+	for (i = 0; i < max_num + 1; i++)
+		if (i - 1 >= 0)
+			counter[i] += counter[i - 1];
+
+	print_array(counter, max_num + 1);
+	free(counter);
+}
