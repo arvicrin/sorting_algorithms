7fc5e55116358a48bda3676f702fcc24f377f89c
arvicrin <1231@holbertonschool.com>
2020-02-05 22:57:27 +0000

    Reduce 1 for statement

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,13 +8,15 @@
   */
 void counting_sort(int *array, size_t size)
 {
-	int counter[10000], c_p[10000], max_num = array[0], i = 0, si = size;
+	int counter[10000], c_p[10000], c_a[10000];
+	int max_num = array[0], i = 0, si = size, num = 0;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < si; i++)
 	{
+		c_a[i] = array[i];
 		if (array[i] > max_num)
 			max_num = array[i];
 		counter[array[i]] += 1;
@@ -23,14 +25,14 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < max_num + 1; i++)
 		if (i - 1 >= 0)
 			counter[i] += counter[i - 1];
+
 	print_array(counter, max_num + 1);
 
 	for (i = 0; i < si; i++)
 	{
-		c_p[counter[array[i]] - 1] = array[i];
-		counter[array[i]] -= 1;
+		num = counter[c_a[i]] - 1;
+		c_p[num] = c_a[i];
+		array[num] = c_p[num];
+		counter[c_a[i]] -= 1;
 	}
-	for (i = 0; i < si; i++)
-		array[i] = c_p[i];
-
 }
