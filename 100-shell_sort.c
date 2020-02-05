a571774a4da9be95ea14e1fce2f1814e3c5112c6
arvicrin <1231@holbertonschool.com>
2020-02-05 20:34:08 +0000

    Fix condition

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,22 +1,6 @@
 #include "sort.h"
 
 /**
-  * swap_s - swap two element of a list
-  * @array: array
-  * @i: index
-  * @j: j
-  * Return: nothing
-  */
-void swap_s(int *array, int i, int j)
-{
-	int copy_num = 0;
-
-	copy_num = array[i];
-	array[i] = array[j];
-	array[j] = copy_num;
-}
-
-/**
   * shell_sort - Selection sort
   * @array: array
   * @size: size
@@ -24,7 +8,7 @@ void swap_s(int *array, int i, int j)
   */
 void shell_sort(int *array, size_t size)
 {
-	int gap = 1, i = 0, j = 0, si = size;
+	int gap = 1, i = 0, j = 0, si = size, save_num = 0;
 
 	while (gap < si / 3)
 		gap = (gap * 3) + 1;
@@ -32,12 +16,13 @@ void shell_sort(int *array, size_t size)
 	while (gap > 0)
 	{
 		for (i = gap; i < si; i++)
-			for (j = i; j > 0 && array[j] < array[j - gap]; j -= gap)
-			{
-				swap_s(array, j, j - gap);
-				if (j - gap - gap < 0)
-					break;
-			}
+		{
+			save_num = array[i];
+			for (j = i; j >= gap && save_num < array[j - gap]; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = save_num;
+		}
+
 		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
