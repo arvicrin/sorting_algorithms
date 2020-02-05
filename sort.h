dee587fe622f07c6f560e0bdd69b6e241cec48a6
danrocus1994 <danrodcastillo1994@gmail.com>
2020-02-04 19:33:28 -0500

    das

--- sort.h
+++ sort.h
@@ -23,4 +23,5 @@ void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
 #endif
