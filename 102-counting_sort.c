31a9f457ba52de7f3b1c8decf101c3a9b6f8773d
arvicrin <1231@holbertonschool.com>
2020-02-06 03:01:06 +0000

    Batman help me

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,44 +8,41 @@
   */
 void counting_sort(int *array, size_t size)
 {
-	int *counter = NULL, *c_a = NULL;
-	int max_num = array[0], i = 0, si = size, num = 0;
+	int *counter = NULL, *c_p = NULL, max_num = array[0], i = 0, si = size;
 
 	if (array == NULL || size < 2)
 		return;
 
-	c_a = malloc(sizeof(int) * size);
-	if (!c_a)
-		return;
-
 	for (i = 0; i < si; i++)
-	{
-		c_a[i] = array[i];
 		if (array[i] > max_num)
 			max_num = array[i];
-	}
+
 	counter = malloc(sizeof(int) * (max_num + 1));
 	if (!counter)
-	{
-		free(c_a);
 		return;
-	}
+
+	for (i = 0; i < max_num + 1; i++)
+		counter[i] = 0;
+
 	for (i = 0; i < si; i++)
 		counter[array[i]] += 1;
 
-	for (i = 0; i < max_num + 1; i++)
-		if (i - 1 >= 0)
-			counter[i] += counter[i - 1];
-
+	for (i = 1; i < max_num + 1; i++)
+		counter[i] += counter[i - 1];
 	print_array(counter, max_num + 1);
+
+	c_p = malloc(sizeof(int) * size);
+	if (!c_p)
+		return;
 
 	for (i = 0; i < si; i++)
 	{
-		num = counter[c_a[i]] - 1;
-		array[num] = c_a[i];
-		counter[c_a[i]] -= 1;
+		c_p[counter[array[i]] - 1] = array[i];
+		counter[array[i]] -= 1;
 	}
+	for (i = 0; i < si; i++)
+		array[i] = c_p[i];
 
-	free(c_a);
 	free(counter);
+	free(c_p);
 }
